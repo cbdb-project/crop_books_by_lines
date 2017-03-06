3bbfd19a0c0ccbf562bdc72a9a273642ec783e1c
sudoghut <sudospace@gmail.com>
2017-03-06 01:08:28 -0500

    update

---
+++ a.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import numpy
+import math
+
+im = Image.open('1.tif')
+pixels = list(im.getdata())
+width, height = im.size
+pixels_top_bottom = [pixels[i * width:(i + 1) * width] for i in range(height)]
+pixels_left_right = [i.count(0) for i in list(zip(*pixels_top_bottom[::-1]))]
+pixels_left_right_diff = [abs(j-i) for i,j in zip(pixels_left_right, pixels_left_right[1:])]
+
+print(pixels_left_right_diff)
+
+#im_new = im.crop((0,0,100,100))
+#im_new.save("./a.tif", "TIFF")
